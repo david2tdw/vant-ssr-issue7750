7a537663bd6b5806c83034e49e08096ec1aee50f
Da Wei <dawei.tang@roadshare.com>
2020-12-17 15:57:26 +0800

    https://github.com/youzan/vant/issues/7750

---
+++ pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <Swipe class="mobile-home-swipe" :autoplay="3000" indicator-color="#32C5FF" :loop="true">
+      <SwipeItem>
+        <div class="solution-item">
+          <div>
+            智能制造解决方案
+          </div>
+          <div>智能制造解决方案智能制造解决方案智能制造解决方案智能制造解决方案智能制造解决方案智能制造解决方案智能制造解决方案智能制造解决方案智能制造解决方案智能制造解决方案。 </div>
+        </div>
+      </SwipeItem>
+      <SwipeItem>
+        <div class="solution-item">
+          <div>
+            大数据服务与应用
+          </div>
+          <div>大数据服务与应用大数据服务与应用大数据服务与应用大数据服务与应用大数据服务与应用大数据服务与应用大数据服务与应用大数据服务与应用大数据服务与应用大数据服务与应用。</div>
+        </div>
+      </SwipeItem>
+      <SwipeItem>
+        <div class="solution-item">
+          <div>
+            工业互联网解决方案
+          </div>
+          <div>工业互联网解决方案工业互联网解决方案工业互联网解决方案工业互联网解决方案工业互联网解决方案工业互联网解决方案工业互联网解决方案工业互联网解决方案工业互联网解决方案。 </div>
+        </div>
+      </SwipeItem>
+    </Swipe>
+  </div>
+</template>
+
+<script>
+import { Swipe, SwipeItem } from 'vant'
+export default {
+  name: 'Xxx',
+  layout: 'mobile-layout',
+  components: {
+    Swipe,
+    SwipeItem
+  },
+  data () {
+    return {}
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="stylus">
+</style>
